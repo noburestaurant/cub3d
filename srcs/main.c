9091988744a7121c6dea2dd8cc7d8c5d7781704a
Hayanobu Nakayama <hnakayam@c3r2s1.42tokyo.jp>
2024-12-16 23:56:38 +0900

    fix; need to use the mlx_clear_window function

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hnakayam <hnakayam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:00:37 by hnakayam          #+#    #+#             */
-/*   Updated: 2024/12/16 23:41:57 by hnakayam         ###   ########.fr       */
+/*   Updated: 2024/12/16 23:55:22 by hnakayam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,9 +121,10 @@ int	main(int argc, char **argv)
 	if (vars.win == NULL)
 		error_message_and_free(&vars, "Unexpected Error: mlx\n", 1);
 	get_img(&vars);
-	render_map(&vars);
-	print_player(&vars);
-	render_rays(&vars);
+	// render_map(&vars);
+	// print_player(&vars);
+	// render_rays(&vars);
+	// print_orientation(&vars);
 	render_field_of_view(&vars);
 	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_hook(vars.win, 17, 0, window_close, &vars);
